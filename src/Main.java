fb81d2f8121231524e82a74de3680760cd045015
MariaDzha <m_zudina@inbox.ru>
2021-11-28 12:40:27 +0300

    Project added

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAccount = 100;
+        int refill = 1100;
+        int totalAmount;
+        int bonusAmount = (refill / 100);
+        if
+        (refill > 1000) {
+            totalAmount = currentAccount + refill + bonusAmount;
+        } else {
+            totalAmount = currentAccount + refill;
+            bonusAmount = 0;
+        }
+        System.out.println("Общая сумма: " + totalAmount);
+        System.out.println("Сумма бонусов: " + bonusAmount);
+    }
+}
